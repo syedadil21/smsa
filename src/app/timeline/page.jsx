7ff1f08b2048841c91ce48bf1badcd742424a44b
Syed Adil <syed_adil21@live.com>
2024-04-07 17:42:36 +0500

    Second

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
   return (
     <div className="w-full p-10 px-40 flex flex-col items-start bg-[#C8C8BE] h-[100vh] py-40">
         <div className='flex my-5 items-center'>
-            <Image src={img2} width={200} objectFit='cover' className='w-32 mx-20 h-32 rounded-full'/>
+            <Image alt={"ProfilePicture"} src={img2} width={200} objectFit='cover' className='w-32 mx-20 h-32 rounded-full'/>
             <div className='flex  flex-col'>
                 <p className='text-sm font-bold'>Emmanuel</p>
                 <p className='my-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt natus sint, odit adipisci suscipit rerum ullam obcaecati laudantium tempora. Non natus maxime voluptatem enim fugit. Minus minima dolorum fugit.</p>
@@ -14,7 +14,7 @@ const page = () => {
             </div>
         </div>
         <div className='flex my-5 items-center'>
-            <Image src={img1} width={200} objectFit='cover' className='w-32 mx-20 h-32 rounded-full'/>
+            <Image alt={"ProfilePicture"} src={img1} width={200} objectFit='cover' className='w-32 mx-20 h-32 rounded-full'/>
             <div className='flex  flex-col'>
                 <p className='text-sm font-bold'>Angela</p>
                 <p className='my-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt natus sint, odit adipisci suscipit rerum ullam obcaecati laudantium tempora. Non natus maxime voluptatem enim fugit. Minus minima dolorum fugit.</p>
