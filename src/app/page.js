66f75ba4026ddd21c39c34f016beceecd25ca21d
Syed Adil <syed_adil21@live.com>
2024-05-08 00:04:28 +0500

    Working Project

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,33 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 export default function Home() {
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const router = useRouter()
+
+  const handleLogin = () => {
+    if(email === "test1@gmail.com" || email === "test2@gmail.com")
+    {
+      if(password==='test123')
+      {
+        router.push("/timeline");
+        localStorage.setItem("email", email);
+      }
+      else
+      {
+        alert("Incorrect email or password, try again!")
+      }
+    }
+    else
+    {
+      alert("Incorrect email or password, try again!") 
+    }
+  }
   return (
     <>
     <div className="w-full  flex flex-col items-center bg-[#C8C8BE] h-[100vh] py-40">
@@ -8,10 +35,10 @@ export default function Home() {
       <h1 className="text-5xl font-bold">Social Media Sentiment Analysis</h1>
       <h2 className="text-2xl font-light">Get to know the intent of the user</h2>
       <div className=" flex flex-col my-2">
-        <input type="text" placeholder="Email" className="border-2 my-2 border-gray-600 rounded-xl p-2" />
-        <input type="text" placeholder="Password" className="border-2 my-2 border-gray-600 rounded-xl p-2" />
+        <input type="text" placeholder="Email" value={email} onChange={(e)=>{setEmail(e.target.value)}} className="border-2 my-2 border-gray-600 rounded-xl p-2" />
+        <input type="password" placeholder="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}} className="border-2 my-2 border-gray-600 rounded-xl p-2" />
       </div>
-      <input type="button" value="Login" className="bg-white rounded-full p-3 hover:cursor-pointer px-10" />
+      <input type="button" value="Login" onClick={handleLogin} className="bg-white rounded-full p-3 hover:cursor-pointer px-10" />
     </div>
     </>
   );
--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,22 +1,32 @@
 'use client'
 
 import Image from 'next/image'
-import img1 from '../../../public/capture.png'
-import img2 from '../../../public/capture1.png'
-import { useEffect } from 'react';
+// import img1 from '../../../public/capture.png'
+// import img2 from '../../../public/capture1.png'
+// import img3 from '../../../public/capture.png'
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+// const samplePosts = []
 
 const page = () => {
 
+    const [posts, setPosts] = useState([])
+    const [email, setEmail] = useState("") 
+    const [enteredContent, setEnteredContent] = useState("")
+    const router = useRouter();
+
     useEffect(() => {
       
-        
-
+        const item = localStorage.getItem("email");
+        setEmail(item);
+        console.log(item);
         
     }, [])
     
 
-    const getResult = async () => {
+    const getResult = async (content) => {
 
         const options = {
           method: 'POST',
@@ -28,39 +38,61 @@ const page = () => {
           },
           data: {
             language: 'english',
-            text: `I hate myself`
+            text: content
           }
         };
         
         try {
             const response = await axios.request(options);
-            console.log(response.data);
+            console.log(response.data.sentiment);
+            return response.data.sentiment;
         } catch (error) {
             console.error(error);
         }
 
     }
 
+    const handleAddPost = async () => {
+        if(enteredContent !== "")
+        {
+            const sentiment = await getResult(enteredContent);
+            setPosts([...posts, {['email']: email, ['content']: enteredContent, ['sentiment']: sentiment }])
+            setEnteredContent("")
+            console.log(posts);
+        }
+        else
+            alert("Enter something to add a post")
+    }
 
 
+    const handleLogout = async () => {
+        localStorage.setItem("email", "");
+        router.push('/')
+    }
   return (
-    <div className="w-full p-10 px-40 flex flex-col items-start bg-[#C8C8BE] h-[100vh] py-40">
-        <div className='flex my-5 items-center'>
-            <Image alt={"ProfilePicture"} src={img2} width={200} objectFit='cover' className='w-32 mx-20 h-32 rounded-full'/>
-            <div className='flex  flex-col'>
-                <p className='text-sm font-bold' onClick={()=>{getResult();}}>Emmanuel</p>
-                <p className='my-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt natus sint, odit adipisci suscipit rerum ullam obcaecati laudantium tempora. Non natus maxime voluptatem enim fugit. Minus minima dolorum fugit.</p>
-                <p className='font-semibold'>Negative!</p>
-            </div>
+    <div className="w-full  px-40 flex flex-col items-start bg-[#C8C8BE] py-20 h-full">
+        <h1 className='w-full text-center text-8xl my-10 font-semibold'>Timeline</h1>
+
+        {
+            posts.map((item)=> (
+                <div className='flex my-5 items-center'>
+                    <img alt={"ProfilePicture"} src='../../../public/capture.png' {...(item.email === "test1@gmail.com" ? {src: 'https://alqlspfwemlbqdmocscp.supabase.co/storage/v1/object/public/HaadiAppBucket/UserProfilePicture/capture.png'} : {src: 'https://alqlspfwemlbqdmocscp.supabase.co/storage/v1/object/public/HaadiAppBucket/UserProfilePicture/capture1.png?t=2024-05-07T18%3A40%3A20.296Z'})}  width={200} className='w-32 mx-20 h-32 rounded-full'/>
+                    <div className='flex  flex-col'>
+                        <p className='text-sm font-bold'>{item.email==="test1@gmail.com" ? "Angela" : "Emmanuel"}</p>
+                        <p className='my-3'>{item.content}</p>
+                        <p className='font-semibold'>{item.sentiment}</p>
+                    </div>
+                </div>
+            ))
+        }
+
+        <div className='flex flex-col w-full items-center justify-center'>
+            <input type="text" placeholder="Enter Content" value={enteredContent} onChange={(e)=>{setEnteredContent(e.target.value)}} className="border-2 my-2 border-gray-600 rounded-xl p-2" />
+
+            <input type="button" value="Add a Post" onClick={handleAddPost} className="bg-white rounded-full my-2 p-3 hover:cursor-pointer px-10" />
+            <input type="button" value="Logout" onClick={handleLogout} className="bg-white rounded-full p-3 my-2 hover:cursor-pointer px-10" />
         </div>
-        <div className='flex my-5 items-center'>
-            <Image alt={"ProfilePicture"} src={img1} width={200} objectFit='cover' className='w-32 mx-20 h-32 rounded-full'/>
-            <div className='flex  flex-col'>
-                <p className='text-sm font-bold'>Angela</p>
-                <p className='my-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt natus sint, odit adipisci suscipit rerum ullam obcaecati laudantium tempora. Non natus maxime voluptatem enim fugit. Minus minima dolorum fugit.</p>
-                <p className='font-semibold'>Positive!</p>
-            </div>
-        </div>
+
     </div>
   )
 }
